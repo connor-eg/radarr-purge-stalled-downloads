295fad5344aca695d1f03b0979c15870c5b07972
Connor Garcia <connoregarcia@gmail.com>
2023-09-20 10:44:53 -0500

    Update get_queue.py

--- Radarr/get_queue.py
+++ Radarr/get_queue.py
@@ -1,7 +1,7 @@
 from pyarr import RadarrAPI
 import json
 # If this script is on the same machine as Radarr, you can use the loopback address below.
-# Actually, I've had no need to test this myself, but this whole setup should actually work over the Internet.
+# Actually, I've had no need to test this myself, but this whole setup should work over the Internet.
 # In either case, you'll need to specify the port number that Radarr is using.
 host_url = 'http://127.0.0.1:12345/'
 # Get this API key from your Radarr's web UI.
