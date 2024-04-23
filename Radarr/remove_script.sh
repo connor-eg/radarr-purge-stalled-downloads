3aab08ba98d9db60a359bc61e2cfdb0a5505ba66
Connor Garcia <connoregarcia@gmail.com>
2024-04-22 19:36:36 -0500

    Update remove_script.sh
    Tidied up the code and comments a bit

--- Radarr/remove_script.sh
+++ Radarr/remove_script.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
 # Uncomment and fix the line below if you're having to set this up in your own virtualenv environment.
+# If you don't know what that is, then it doesn't apply to you.
 # source /**YOUR VIRTUALENV DIRECTORY**/virtualenv/bin/activate
 
-# You'll just need to point this at the get_queue.py script that sits on your machine.
-parsed=($(python /**YOUR SCRIPT DIRECTORY**/get_queue.py \
-| jq -r '[.records[] | select(.errorMessage == "The download is stalled with no connections") | .id] | @sh'))
-# If you're on a non-English localization of Radarr, change the text above to select the correct error.
-# Yeah, it's pretty janky, but without more canonical error codes given by Radarr this is just how it is.
+# You'll need to point this at the get_queue.py script that sits on your machine.
+
+scriptDir="."
+parsed=($(python ${scriptDir}/del_queue.py | jq -r '[.records[] | select(.errorMessage == "The download is stalled with no connections") | .id] | @sh'))
+# If you're using a non-English localization of Radarr, change the text in the string here ^^^ to the error you get in the same situation.
+# I know that's pretty bad but Radarr doesn't give proper error codes so that's how it has to be done.
 
 for id in "${parsed[@]}"
 do
-        # Update this as well to the correct directory.
-        python /**YOUR SCRIPT DIRECTORY**/del_queue.py "$id"
+        python ${scriptDir}/del_queue.py "$id"
 done
